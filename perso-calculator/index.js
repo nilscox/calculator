944d45753154e94bc57da1dda68498b9246731ed
Nils <nils@epita.fr>
2021-12-03 19:03:23 +0100

    feat(calculator): implement the binary operations

---
+++ perso-calculator/index.js
@@ -0,0 +1,90 @@
+const getElement = (className) => document.getElementsByClassName(className)[0];
+
+const result = getElement("result");
+const reset = getElement("reset");
+const plusMinus = getElement("plus-minus");
+const percent = getElement("percent");
+const division = getElement("division");
+const product = getElement("product");
+const minus = getElement("minus");
+const plus = getElement("plus");
+const dot = getElement("dot");
+const equals = getElement("equals");
+
+const numbers = Array(9)
+  .fill(0)
+  .map((_, n) => getElement(n + 1));
+
+numbers.unshift(getElement("zero"));
+
+let left = "";
+let right = "";
+let operator = null;
+
+const setResult = (value) => {
+  result.innerText = String(Number(value));
+};
+
+const onNumberClick = (num) => {
+  if (!left) {
+    left = num;
+    setResult(left);
+  } else if (!operator) {
+    left += num;
+    setResult(left);
+  } else {
+    right += num;
+    setResult(right);
+  }
+};
+
+const onOperatorClick = (op) => {
+  if (operator) {
+    computeResult();
+  }
+
+  operator = op;
+};
+
+const computeResult = () => {
+  if (!left) {
+    return alert("left operand is not set");
+  }
+
+  if (!operator) {
+    return alert("operator is not set");
+  }
+
+  if (!left) {
+    return alert("right operand is not set");
+  }
+
+  const a = Number(left);
+  const b = Number(right);
+
+  if (operator === "/" && b === 0) {
+    return alert("division by zero cannot be computed");
+  }
+
+  const operationResult = {
+    "+": a + b,
+    "-": a - b,
+    X: a * b,
+    "/": a / b,
+  }[operator];
+
+  setResult(operationResult);
+
+  left = operationResult;
+  right = "";
+};
+
+for (const number of numbers) {
+  number.addEventListener("click", () => onNumberClick(number.textContent));
+}
+
+for (const operatorElement of [plus, minus, product, division]) {
+  operatorElement.addEventListener("click", () =>
+    onOperatorClick(operatorElement.textContent)
+  );
+}
